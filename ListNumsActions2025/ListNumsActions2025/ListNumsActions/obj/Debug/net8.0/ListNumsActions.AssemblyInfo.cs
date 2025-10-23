e8691966cfc7db06e809d98296945065e1c7a461
Alexandraa2021PMG <a.todorova2021@pmg-pernik.com>
2025-10-23 12:35:57 +0300

    del

--- ListNumsActions2025/ListNumsActions2025/ListNumsActions/obj/Debug/net8.0/ListNumsActions.AssemblyInfo.cs
+++ ListNumsActions2025/ListNumsActions2025/ListNumsActions/obj/Debug/net8.0/ListNumsActions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListNumsActions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d7be1b29fc7fb7f268be850ebb2a251e3b36d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListNumsActions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListNumsActions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
