0d2fa3456a2e48ada8abf841b4fb44d1a08e17f8
VANJOO322 <i.bozhichkov2021@pmg-pernik.com>
2025-10-23 13:56:10 +0300

    evens

--- ListNumsActions2025/ListNumsActions2025/ListNumsActions/obj/Debug/net8.0/ListNumsActions.AssemblyInfo.cs
+++ ListNumsActions2025/ListNumsActions2025/ListNumsActions/obj/Debug/net8.0/ListNumsActions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListNumsActions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25647c2e5e513f2a3db0ed1f836bba982317a1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25accbefe97320d8683ef238b4098f49bd1507d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListNumsActions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListNumsActions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
